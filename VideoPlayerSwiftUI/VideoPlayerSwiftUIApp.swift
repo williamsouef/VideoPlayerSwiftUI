b7209712383fd39185adeb6d185c10ecb2808cd4
WILLIAM SOUEF <williamsouef@gmail.com>
2022-12-28 11:31:46 +0100

    Initial Commit

---
+++ VideoPlayerSwiftUI/VideoPlayerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerSwiftUIApp.swift
+//  VideoPlayerSwiftUI
+//
+//  Created by William Souef on 28/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
